4067c1a9f001214e18640e1985b780bf8acd7bac
Phaneendra <phaneendra.neelamraju99@gmail.com>
2022-07-01 18:32:45 +0530

    Horizantal Bar Chart Changes

--- src/app/components/horizantalbarchart/horizantalbarchart.component.ts
+++ src/app/components/horizantalbarchart/horizantalbarchart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { ChartOptions, ChartType } from "chart.js";
+import ChartDataLabels from 'chartjs-plugin-datalabels';
 
 @Component({
   selector: "app-horizantalbarchart",
@@ -7,55 +8,112 @@ import { ChartOptions, ChartType } from "chart.js";
   styleUrls: ["./horizantalbarchart.component.scss"],
 })
 export class HorizantalbarchartComponent implements OnInit {
-  public currentBarChartOptions: ChartOptions = {
+  public currentBarChartOptions: any = {
     responsive: true,
-    scales: {
-      xAxes: [{
-        ticks: {
-          beginAtZero: true,
-          min: 0
+    tooltips: {
+      mode: "nearest",
+    },
+    // plugins: [
+    //   ChartDataLabels
+    // ],
+    plugins: {
+      datalabels: {
+        formatter:  function(value, context) {
+          return value + '%';
         }
-      }]
+      }
+    },
+    scales: {
+      yAxes: [
+        {
+          categoryPercentage: 10,
+          barPercentage: 2.0,
+          barThickness: 12,
+          maxBarThickness: 15,
+          minBarLength: 5,
+        }
+      ],
+      xAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+            min: 0,
+          },
+
+        },
+      ],
     },
     title: {
       display: true,
       text: "Current",
-      position: "bottom"
+      position: "bottom",
     },
   };
-  public recommededBarChartOptions: ChartOptions = {
+  public recommededBarChartOptions: any = {
     responsive: true,
-    scales: {
-      xAxes: [{
-        ticks: {
-          beginAtZero: true,
-          min: 0
+    plugins: {
+      // ChartDataLabels,
+      datalabels: {
+        formatter:  function(value, context) {
+          return value + '%';
         }
-      }]
+      }
+    },
+    scales: {
+      yAxes: [
+        {
+          categoryPercentage: 1,
+          barPercentage: 2,
+          barThickness: 12,
+          maxBarThickness: 15,
+          minBarLength: 5,
+        }
+      ],
+      xAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+            min: 0,
+          },
+        },
+      ],
+    },
+    tooltips: {
+      mode: "nearest",
     },
     title: {
       display: true,
       text: "Recommended",
-      position: "bottom"
+      position: "bottom",
     },
   };
   public barChartType: any = "horizontalBar";
   public barChartLegend = false;
 
   public recommededBarChartData: any[] = [
-    { data: [20, 20, 30, 40], label: "SL1" },
-    { data: [19, 25, 33, 45], label: "SL2" },
-    { data: [12, 23, 39, 70], label: "SL3" },
-    { data: [22, 29, 35, 55], label: "SL4" },
-    { data: [42, 44, 32, 65], label: "SL5" },
+    { data: [20, 20, 30, 40], label: "SL1",
+                     },
+    { data: [19, 25, 33, 45], label: "SL2",
+                    },
+    { data: [12, 23, 39, 70], label: "SL3",
+                    },
+    { data: [22, 29, 35, 55], label: "SL4",
+                    },
+    { data: [42, 44, 32, 65], label: "SL5",
+                    },
   ];
 
   public currentBarChartData: any[] = [
-    { data: [20, 20, 30, 40], label: "SL1" },
-    { data: [19, 25, 33, 45], label: "SL2" },
-    { data: [12, 23, 39, 70], label: "SL3" },
-    { data: [22, 29, 35, 55], label: "SL4" },
-    { data: [42, 44, 32, 65], label: "SL5" },
+    { data: [20, 20, 30, 40], label: "SL1",
+                    },
+    { data: [19, 25, 33, 45], label: "SL2",
+                    },
+    { data: [12, 23, 39, 70], label: "SL3",
+                    },
+    { data: [22, 29, 35, 55], label: "SL4",
+                    },
+    { data: [42, 44, 32, 65], label: "SL5" ,
+                    categoryPercentage: 10.0,},
   ];
 
   public barChartLabels: string[] = [
